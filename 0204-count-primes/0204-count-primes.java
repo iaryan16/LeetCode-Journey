5db496b0600bb6f20f707f3c911064790a24a097
iaryan16 <iaryan.dev@outlook.com>
2025-09-05 21:18:21 +0530

    Time: 103 ms (35.25%), Space: 49.3 MB (47.46%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+        if (n <= 2) return 0;
+        
+        boolean arr[] = new boolean[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = true;
+        }
+
+        arr[0] = false;
+        arr[1] = false;
+
+        for(int i = 2; i * i < n ; i++) {
+            if(arr[i]) {
+                for(int j = i * i; j < n; j += i) {
+                    arr[j] = false;
+                }
+            }
+            
+        }
+        int c = 0;
+        for(int i=2; i<n; i++) {
+            if(arr[i])  
+                c++;
+        }
+        return c;
+    }
+}
