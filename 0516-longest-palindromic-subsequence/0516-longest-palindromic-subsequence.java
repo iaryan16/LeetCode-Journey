3288da6a595acadc70ddf21065142288b6896770
iaryan16 <iaryan.dev@outlook.com>
2025-10-04 21:56:37 +0530

    Time: 48 ms (55.02%), Space: 47.3 MB (86.59%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,39 +1,46 @@
 class Solution {
     public int longestPalindromeSubseq(String s1) {
         
+        String s2 = reverse(s1);
+
         int n = s1.length();
-        int dp[][] = new int[n+1][n+1];
+        int m = s2.length();
+
+        int dp[][] = new int[n+1][m+1];
 
         for(int i=0; i<=n; i++) {
-            for(int j=0; j<=n; j++) {
-                dp[i][j] = -1;
+            for(int j=0; j<=m; j++) {
+                    dp[i][j] = -1;
             }
         }
-
-        int i=0;
-        int j = s1.length()-1;
-
-        return subseq(s1, i, j, dp);
-
+        return subseq(s1, s2, n, m, dp);
     }
 
-    public int subseq(String s, int i, int j, int dp[][]) {
-        if(i > j) {
+    public int subseq(String s1, String s2, int n, int m, int dp[][]) {
+        if(n==0 || m==0) {
             return 0;
         }
-        if(i == j) {
-            return 1;
-        }
 
-        if(dp[i][j] != -1) {
-            return dp[i][j];
+        if(dp[n][m] != -1) {
+            return dp[n][m];
         }
-
-        if(s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = 2 + subseq(s, i+1, j-1, dp);
+        
+        if(s1.charAt(n-1) == s2.charAt(m-1)) {
+            return dp[n][m] = 1 + subseq(s1, s2, n-1, m-1, dp);
         } else {
-            return dp[i][j] = Math.max(subseq(s, i+1, j, dp), subseq(s, i, j-1, dp));
-        }   
+            return dp[n][m] = Math.max(subseq(s1, s2, n-1, m, dp), subseq(s1, s2, n, m-1, dp));
+        }
+    }
+
+    public static String reverse(String s) {
+        String rev = "";
+        int n = s.length()-1;
+        while(n>=0) {
+            char ch = s.charAt(n);
+            rev += ch;
+            n--;
+        }
+        return rev;
     }
 
 } 
