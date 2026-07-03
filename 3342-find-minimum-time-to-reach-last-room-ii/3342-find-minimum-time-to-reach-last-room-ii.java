39cf27d32a8fe54c6ec7096523fd209164c0f426
iaryan16 <iaryan.dev@outlook.com>
2026-07-03 14:55:44 +0530

    Time: 214 ms (59.76%), Space: 184.5 MB (36.69%) - LeetHub

---
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        return bfs(moveTime);
+    }
+
+    public int bfs(int moveTime[][]) {
+
+        int n = moveTime.length;
+        int m = moveTime[0].length;
+        // boolean visited[][] = new boolean[n][m];
+        long distance[][] = new long[n][m];
+        for (int i = 0; i < n; i++)
+            Arrays.fill(distance[i], Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[2] - b[2];
+        });
+
+        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        distance[0][0] = 0;
+        pq.offer(new int[] {0, 0, 0});
+        
+        while (!pq.isEmpty()) {
+            int size = pq.size();
+
+            for (int i = 0; i < size; i++) {
+                int curr[] = pq.poll();
+                int x = curr[0];
+                int y = curr[1];
+                int time = curr[2];
+                if (x == n - 1 && y == m - 1) {
+                    return time;
+                }
+
+                if (time > distance[x][y])
+                    continue;
+
+                for (int dir[] : directions) {
+                    int newX = x + dir[0];
+                    int newY = y + dir[1];
+
+                    if (newX < 0 || newX >= n || newY < 0 || newY >= m)
+                        continue;
+                    int alter = (x + y) % 2 == 0 ? 1 : 2;
+                    int newTime = Math.max(time, moveTime[newX][newY]) + alter;
+
+                    if (distance[newX][newY] > newTime) {
+                        distance[newX][newY] = newTime;
+                        pq.offer(new int[] { newX, newY, newTime });
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
