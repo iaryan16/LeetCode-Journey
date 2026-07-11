1044744e6811b6d9b416a747e8f06bd829fea143
iaryan16 <iaryan.dev@outlook.com>
2026-07-11 11:59:39 +0530

    Time: 17 ms (15.25%), Space: 47.7 MB (58.78%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,14 +1,15 @@
 class Solution {
     boolean visited[];
+
     public int countCompleteComponents(int n, int[][] edges) {
-        
+
         Map<Integer, List<Integer>> map = new HashMap<>();
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             map.put(i, new ArrayList<>());
         }
 
-        for(int i=0; i<edges.length; i++) {
+        for (int i = 0; i < edges.length; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
             map.get(u).add(v);
@@ -18,8 +19,8 @@ class Solution {
         visited = new boolean[n];
 
         int c = 0;
-        for(int i=0; i<n; i++) {
-            if(!visited[i] && solve(i, map)) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && solve(i, map)) {
                 c++;
             }
         }
@@ -27,36 +28,94 @@ class Solution {
         return c;
     }
 
+    // public boolean solve(int node, Map<Integer, List<Integer>> map) {
+
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.offer(node);
+    //     int nodes = 0, edges = 0;
+    //     while (!q.isEmpty()) {
+    //         int curr = q.poll();
+    //         if (visited[curr])
+    //             continue;
+    //         visited[curr] = true;
+    //         nodes++;
+    //         for (int i : map.get(curr)) {
+    //             if (!visited[i]) {
+    //                 q.offer(i);
+    //                 edges++;
+    //             }
+    //         }
+    //     }
+
+    //     int reqEdges = (nodes * (nodes - 1)) / 2;
+
+    //     if (edges == reqEdges) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // public boolean solve(int node, Map<Integer, List<Integer>> map) {
+
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.offer(node);
+
+    //     int nodes = 0;
+    //     int edges = 0;
+
+    //     while (!q.isEmpty()) {
+    //         int curr = q.poll();
+
+    //         if (visited[curr])
+    //             continue;
+
+    //         visited[curr] = true;
+    //         nodes++;
+
+    //         edges += map.get(curr).size();
+
+    //         for (int next : map.get(curr)) {
+    //             if (!visited[next]) {
+    //                 q.offer(next);
+    //             }
+    //         }
+    //     }
+
+    //     edges /= 2;
+
+    //     int reqEdges = nodes * (nodes - 1) / 2;
+
+    //     return edges == reqEdges;
+    // }
+
     public boolean solve(int node, Map<Integer, List<Integer>> map) {
 
-    Queue<Integer> q = new LinkedList<>();
-    q.offer(node);
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> component = new ArrayList<>();
 
-    int nodes = 0;
-    int edges = 0;
+        q.offer(node);
+        visited[node] = true;
 
-    while (!q.isEmpty()) {
-        int curr = q.poll();
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            component.add(curr);
 
-        if (visited[curr])
-            continue;
-
-        visited[curr] = true;
-        nodes++;
-
-        edges += map.get(curr).size();
-
-        for (int next : map.get(curr)) {
-            if (!visited[next]) {
-                q.offer(next);
+            for (int next : map.get(curr)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.offer(next);
+                }
             }
         }
+
+        int size = component.size();
+
+        for (int curr : component) {
+            if (map.get(curr).size() != size - 1) {
+                return false;
+            }
+        }
+
+        return true;
     }
-
-    edges /= 2;
-
-    int reqEdges = nodes * (nodes - 1) / 2;
-
-    return edges == reqEdges;
-}
 }
