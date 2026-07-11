90bb358167574599be32a62a719b760278db7387
iaryan16 <iaryan.dev@outlook.com>
2026-07-11 11:52:23 +0530

    Time: 18 ms (14.26%), Space: 47.5 MB (76.09%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -0,0 +1,55 @@
+class Solution {
+    boolean visited[];
+    public int countCompleteComponents(int n, int[][] edges) {
+        
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for(int i=0; i<n; i++) {
+            map.put(i, new ArrayList<>());
+        }
+
+        for(int i=0; i<edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            map.get(u).add(v);
+            map.get(v).add(u);
+        }
+
+        visited = new boolean[n];
+
+        int c = 0;
+        for(int i=0; i<n; i++) {
+            if(!visited[i] && solve(i, map)) {
+                c++;
+            }
+        }
+
+        return c;
+    }
+
+    public boolean solve(int node, Map<Integer, List<Integer>> map) {
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        int nodes = 0, edges = 0;
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            if(visited[curr]) continue;
+            visited[curr] = true;
+            nodes++;
+            for(int i : map.get(curr)) {
+                if(!visited[i]) {
+                    q.offer(i);
+                    edges++;
+                }
+            }
+        }
+
+        int reqEdges = (nodes * (nodes-1)) / 2;
+
+        if(edges == reqEdges) {
+            return true;
+        }
+        return false;
+    }
+}
