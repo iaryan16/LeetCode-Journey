051771ef80a39481c73cc488224ab2788541a3fa
iaryan16 <iaryan.dev@outlook.com>
2026-07-27 09:31:40 +0530

    Time: 114 ms (5.87%), Space: 46.7 MB (16.99%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -6,30 +6,36 @@ class Solution {
 
         int freq[] = new int[26];
 
-        int l=0, r=0, max=0;
+        int l=0, r=0, c=0, max=0;
 
         while(r < n) {
-            freq[arr[r]-'a']++;
+            if(check(arr[r])) c++;
 
             if(r-l+1 > k) {
-                freq[arr[l]-'a']--;
+                if(check(arr[l])) {
+                    c--;
+                }
                 l++;
             }
 
-            max = Math.max(max, check(freq));
+            max = Math.max(max, c);
             r++;
         }
         return max;
     }
 
-    public int check(int[] freq) {
-        int c = 0;
-        for(int i=0; i<freq.length; i++) {
-            if(i == 0 || i == 4 || i == 8 || i == 14 || i == 20) {
-                c += freq[i];
-            }
-        }
-
-        return c;
+    public boolean check(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
+
+    // public int check(int[] freq) {
+    //     int c = 0;
+    //     for(int i=0; i<freq.length; i++) {
+    //         if(i == 0 || i == 4 || i == 8 || i == 14 || i == 20) {
+    //             c += freq[i];
+    //         }
+    //     }
+
+    //     return c;
+    // }
 }
